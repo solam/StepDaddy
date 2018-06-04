8c5ccfc326f48f7f0905741da314d30c60727fb6
keitaro56 <lemeutw@gmail.com>
2018-06-04 20:19:29 +0200

    Rajout du dossier 'graphismes' et des fichiers associés

---
+++ clients/graphismes/scripts/gestionCanvas.js
@@ -0,0 +1,163 @@
+﻿(function()
+{
+	GestionCanvas = function()
+	{
+		var _ctx; //le contexte
+		var _height; //la hauteur du canvas
+		var _width; //la largeur du canvas
+		//var _compteur;
+		//var idFadeOut;
+
+		//const _stepsFadeOut = 1;//10;
+		//var _tempsRafraichissement; //en ms
+
+		this.initialize = function()
+		{
+			var canvas=document.getElementById("canvas");
+			_width=parseInt(canvas.getAttribute("width"));
+			_height=parseInt(canvas.getAttribute("height"));
+			_ctx=canvas.getContext('2d');
+			
+			//_compteur=0;
+			//_tempsRafraichissement = 1000 / bpm / 16;
+			//window.setInterval(dessin, _tempsRafraichissement);
+			return this;
+		}
+
+		//window.onclick = function()
+		//{
+		//	window.setInterval(dessin, rafraichissement);
+		//}
+
+		function dessin()
+		{
+			//if(compteur%rafraichissement==0)
+			//{
+				definitionCouleur(0, 255, 0);
+				dessinerRectangle(-1, -1, -1, -1, true);
+				//definitionCouleur(255, 0, 0);
+				//sinusoideOscillanteT(-1, 200, -1, 200, 30, 8, 50, 100);
+				//sinusoideOscillanteAmplitude(-1, 400, -1, 400, 50, 1, 30, 60);
+				//rectangleOscillant(450, 450, true, 2, 6, 20, 100, 50, 200);
+			//}
+			//else
+			//{
+				fadeOut();
+			//}
+			
+			_compteur++;
+		}
+
+		this.definitionCouleur = function(r, g, b)
+		{
+			_ctx.fillStyle="rgb(" + r.toString() + "," + g.toString() + "," + b.toString() + ")";
+			_ctx.strokeStyle="rgb(" + r.toString() + "," + g.toString() + "," + b.toString() + ")";
+		}
+
+		//(xTopLeft,yTopLeft) : position sommet gauche Rectangle
+		//(w,h) : largeur, hauteur Rectangle
+		this.dessinerRectangle = function(xTopLeft, yTopLeft, w, h, plein)
+		{
+			var valW=(w==-1)?_width:w;
+			var valH=(h==-1)?_height:h;
+			var valX=(xTopLeft==-1)?0:xTopLeft;
+			var valY=(yTopLeft==-1)?0:yTopLeft;
+			
+			if(plein)
+			{
+				_ctx.fillRect(valX, valY, valW, valH);
+			}
+			else
+			{
+				_ctx.strokeRect(valX, valY, valW, valH);
+			}
+		}
+
+		function dessinerRectangleDepuisCentre(xCentre, yCentre, w, h, plein)
+		{
+			dessinerRectangle(xCentre - w/2, yCentre - h/2, w, h, plein);
+		}
+
+		function rectangleTournant(xCentre, yCentre, w, h, plein, )
+		{
+			var w=wMin + (wMax - wMin)*(compteur%(nbPeriodesW*rafraichissement)/(nbPeriodesW*rafraichissement));
+			var h=hMin + (hMax - hMin)*(compteur%(nbPeriodesH*rafraichissement)/(nbPeriodesH*rafraichissement));
+			
+			dessinerRectangleDepuisCentre(xCentre, yCentre, w, h, plein);
+		}
+
+		function rectangleOscillant(xCentre, yCentre, plein, nbPeriodesW, nbPeriodesH, wMin, wMax, hMin, hMax)
+		{
+			var w=wMin + (wMax - wMin)*(compteur%(nbPeriodesW*rafraichissement)/(nbPeriodesW*rafraichissement));
+			var h=hMin + (hMax - hMin)*(compteur%(nbPeriodesH*rafraichissement)/(nbPeriodesH*rafraichissement));
+			
+			dessinerRectangleDepuisCentre(xCentre, yCentre, w, h, plein);
+		}
+
+		function sinusoideOscillanteT(x0, y0, x1, y1, amplitude, nbPeriodes, Tmin, Tmax)
+		{
+			var T = Tmin + (Tmax - Tmin)*(compteur%(nbPeriodes*rafraichissement)/(nbPeriodes*rafraichissement));
+			
+			dessinerSinusoide(x0, y0, x1, y1, T, amplitude);
+		}
+
+		function sinusoideOscillanteAmplitude(x0, y0, x1, y1, T, nbPeriodes, amplitudeMin, amplitudeMax)
+		{
+			var amplitude = amplitudeMin + (amplitudeMax - amplitudeMin)*(compteur%(nbPeriodes*rafraichissement)/(nbPeriodes*rafraichissement));
+			
+			dessinerSinusoide(x0, y0, x1, y1, T, amplitude);
+		}
+
+		function dessinerSinusoide(x0, y0, x1, y1, T, amplitude)
+		{
+			var valX0=(x0==-1)?0:x0;
+			var valY0=(y0==-1)?0:y0;
+			var valX1=(x1==-1)?_width:x1;
+			var valY1=(y1==-1)?_height:y1;
+			
+			var distance = Math.sqrt((valX0-valX1)*(valX0-valX1)+(valY0-valY1)*(valY0-valY1));
+			
+			//on récupère le nombre d'oscillations
+			n=distance/T;
+			
+			//on calcule le vecteur perpendiculaire a la droite (x0, y0) -> (x1, y1)
+			var Xnorm = valX0 - (valY1 - valY0);
+			var Ynorm = valY0 + (valX1 - valX0);
+			
+			//on normalise
+			Xnorm *= amplitude / distance;
+			Ynorm *= amplitude / distance;
+			
+			var x = valX0;
+			var y = valY0;
+			var periodeX = (valX1-valX0)/n;
+			var periodeY = (valY1-valY0)/n;
+			
+			_ctx.beginPath();
+			_ctx.moveTo(x, y);
+			
+			for(var i=1; i<=Math.ceil(n); i++)
+			{
+				_ctx.bezierCurveTo(x + Xnorm, y + Ynorm, x + periodeX/2 + Xnorm, y + periodeY/2 + Ynorm, x + periodeX/2, y + periodeY/2);
+				_ctx.bezierCurveTo(x + periodeX/2 - Xnorm, y + periodeY/2 - Ynorm, x + periodeX - Xnorm, y + periodeY - Ynorm, x + periodeX, y + periodeY);
+				//ctx.bezierCurveTo(x + Xnorm + 1, y + Ynorm - 1, x + periodeX/2 + Xnorm - 1, y + periodeY/2 + Ynorm - 1, x + periodeX/2, y + periodeY/2);
+				//ctx.bezierCurveTo(x + periodeX/2 - Xnorm + 1, y + periodeY/2 - Ynorm + 1, x + periodeX - Xnorm - 1, y + periodeY - Ynorm + 1, x + periodeX, y + periodeY);
+
+				
+				x += periodeX;
+				y += periodeY;
+				
+				_ctx.stroke();
+			}
+			
+			_ctx.closePath();
+		}
+
+		function fadeOut()
+		{
+			_ctx.globalAlpha=(_compteur%_tempsRafraichissement)/_tempsRafraichissement;
+			definitionCouleur(0, 0, 0);
+			dessinerRectangle(-1, -1, -1, -1, true);
+		}
+	};
+}());
---
+++ clients/graphismes/scripts/graphismes.js
@@ -0,0 +1,131 @@
+(function ()
+{
+    Graphismes = function ()
+    {
+        var _canvas; //le canvas qui servira d'affichage
+
+        var _connexion; //l'objet mixr.net.Connexion
+        //var _bpm = 1; //le bpm
+        //var _color = "rgba(0, 0, 0, 0)"; //la couleur associée avec l'instrument
+
+        var _tracks;
+
+        //cette fonction initialise la classe, en établissant la connexion et en définissant les comportements
+        this.initialize = function()
+        {
+            _connexion = new mixr.net.Connection();
+            _connexion.connect(window.SERVER);
+            _connexion.on(mixr.enums.Events.REGISTER, _onRegistered);
+            _connexion.on(mixr.enums.Events.SEQUENCER_BEAT, _onSequencerBeat);
+            _connexion.on(mixr.enums.Events.TRACKS, _onTracks);
+            _connexion.on(mixr.enums.Events.NOTE, _onNote);
+            //_connexion.on(mixr.enums.Events.INSTRUMENT, _onInstrument);
+
+            _canvas = new GestionCanvas().initialize();
+        }
+
+        //cette fonction est appelée à partir du moment où le client est enregistré
+        var _onRegistered = function(data)
+        {
+            console.log('client enregistré', data);
+
+            //if(_isJoinedToRoom)
+            //{
+            //    _conn.joinRoom(_isJoinedToRoom, _onRoomJoined, function (e)
+            //    {
+            //    console.log('Room not joined!', e);
+            //    });
+            //}
+
+            var url = mixr.Utils.parseURL(location.href);
+
+            //http://127.0.0.1:8282/device/?rm/mopo will give mopo as pwd
+            var pwd = url.url.split("/").pop();
+
+            var rmid = JSON.stringify(url.query) + '/' + JSON.stringify(pwd); // rm/mopo
+            var delim = '_';
+            var rmid = url.query.concat(delim).concat(pwd).concat(delim).concat(window.pageId);
+
+            if (rmid)
+            {
+                _connexion.joinRoom(rmid, _onRoomJoined, _onRoomNotJoined);
+            }
+        }
+
+        //fonction appelée en cas de réussite de connexion à la salle
+        var _onRoomJoined = function()
+        {
+            console.log("ok : salle rejointe");
+
+            //on effectue une demande pour avoir le bpm via l'instrument
+            //_connexion.execute(mixr.enums.Events.GET_INSTRUMENT, {});
+
+            //on récupère les tracks des instruments
+            _connexion.execute(mixr.enums.Events.GET_TRACKS, {});
+        }
+
+        //fonction appelée en cas d'échec de connexion à la salle
+        var _onRoomNotJoined = function(e)
+        {
+            console.log("ko : ", e);
+        }
+
+        //fonction appelée lors d'un signal SEQUENCER_BEAT
+        var _onSequencerBeat = function(data)
+        {
+            _canvas.definitionCouleur(0,0,0);
+            _canvas.dessinerRectangle(-1, -1, -1, -1, true);
+            
+            for(var numInstru=0; numInstru<_tracks.length; numInstru++)
+            {
+                for(var numPiste=0; numPiste<_tracks[numInstru].length; numPiste++)
+                {
+                    if(_tracks[numInstru][numPiste] !== 'undefined')
+                    {
+                        _canvas.definitionCouleur(255, 255, 255);
+                        _canvas.dessinerRectangle(numInstru*20, numPiste*30, 10, 10, true);
+
+                        if(_tracks[numInstru][numPiste].notes[data.beat]==1)
+                        {
+                            _canvas.definitionCouleur(4, 12, 240);
+                            _canvas.dessinerRectangle(numInstru*20, numPiste*30, 10, 10, true);
+                        }
+                    }
+                }
+            }
+        }
+
+        //fonction appelée suite à l'envoi de l'appel à GET_INSTRUMENT
+        //var _onInstrument = function(data)
+        //{
+            //_bpm = data.channelInfo.bpm;
+            //_color = data.channelInfo.channelColor;
+
+            //_canvas = new GestionCanvas().initialize(_bpm);
+        //    instrumentEnCours = data;
+        //    console.log(data);
+        //}
+
+        var _onTracks = function(data)
+        {
+            _tracks = data;
+        }
+
+        var _onNote = function(data)
+        {
+            //var numInstru = data.args.id;
+            var numPiste = data.args.trackId.split('-')[1];
+            var numInstru = data.args.trackId.split('-')[0];
+            var numNote = data.args.noteId;
+            var volume = data.args.volume;
+
+            if (typeof _tracks[numInstru][numPiste] !== 'undefined')
+            {
+				_tracks[numInstru][numPiste].notes[numNote] = volume;
+			}
+        }
+    };
+
+    //appel à initialize() au moment de l'appel de graphismes.js
+    new Graphismes().initialize();
+}());
